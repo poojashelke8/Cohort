759d3538813e754650a3d26fda9abfe0110a7563
poojashelke8 <poojabshelke0807@gmail.com>
2024-01-16 13:39:21 +0530

    solved medium problems of 01-js

--- assignments-master/01-js/medium/countVowels.js
+++ assignments-master/01-js/medium/countVowels.js
@@ -7,6 +7,14 @@
 
 function countVowels(str) {
     // Your code here
+    let vowels = ['a','e','i','o','u','A','E','I','O','U']
+    let count = 0
+    for(let i = 0;i<str.length;i++){
+      if(vowels.includes(str[i])){
+        count+=1
+      }
+    }
+    return count
 }
 
 module.exports = countVowels;
