733cab6d93b6b4a2aa7a25546eb48c18df23fe99
poojashelke8 <poojabshelke0807@gmail.com>
2024-01-23 22:38:25 +0530

    implemented all the http methods

--- http-server/index.js
+++ http-server/index.js
@@ -1,28 +1,122 @@
-// const express = require('express')
+// ------------ EXPRESS WEEK 2.2 EXPRESS START--------------
+
+
+// const express  = require("express")
+// const bodyParser = require("body-parser")
 // const app = express()
 // const port = 3000
 
+// app.use(bodyParser.json())
 
-// app.get('/', function(req, res){
-//   res.send('Hello World !')
+// app.get("/hello",function(req,res){
+//     res.send("hello there")
+// })
+// app.post("/",function(req,res){
+//     console.log(req.body)
+//     // res.send("Hi my name is Pooja")
 // })
 
 // app.listen(port)
 
+// -------------- WEEK 2.5 EXPRESS PRACTICE-------------
+// creating http server
+// express 
 
-const express  = require("express")
-const bodyParser = require("body-parser")
+// 1 example - used query parameter
+
+// const express = require("express");
+
+// const app = express();
+
+// function sumData(n) {
+//     let count = 0;
+//     for (let i = 1; i <= n; i++) {
+//         count += i
+//     }
+//     return count;
+// }
+
+// app.get("/", function (req, res) {
+//     const n = req.query.n;
+//     const ans = sumData(n)
+//     res.send("Hi answer is:" + ans);
+// })
+
+// app.listen(3000);
+
+// 2 example of hospital operations
+
+const express = require("express")
+
 const app = express()
-const port = 3000
 
-app.use(bodyParser.json())
+var users = [{
+    name: "john",
+    kidneys: [{
+        healthy: false
+    }]
+},
+// {
+//     name: "jenny",
+//     kidneys: [{
+//         healthy: tru
+//     }]
+// }
+];
 
-app.get("/hello",function(req,res){
-    res.send("hello there")
+app.get("/",function(req,res){
+    const kiData = users[0].kidneys;
+    const kiNum = kiData.length;
+    let count = 0
+    for(let i=0;i<kiData.length;i++){
+        if(kiData[i].healthy){
+            count +=1
+        }
+    }
+    const unhealthyCount = kiNum - count;
+    res.json({
+        kiData,
+        count,
+        unhealthyCount
+    })
 })
+
+app.use(express.json())
 app.post("/",function(req,res){
-    console.log(req.body)
-    // res.send("Hi my name is Pooja")
+     const isHealthy = req.body.isHealthy;
+     users[0].kidneys.push({
+        healthy:isHealthy
+     })
+     res.json({
+        msg:"Done"
+     })
 })
 
-app.listen(port)
+// update unhealthy to healthy
+app.put("/",function(req,res){
+    for(let i=0;i<users[0].kidneys.length;i++){
+        users[0].kidneys[i].healthy = true
+    }
+    res.json({})
+})
+
+// delete unhealthy kidneys make it true
+app.delete("/",function(req,res){
+    const newkidneys = []
+    for(let i=0;i<users[0].kidneys.length;i++){
+        if(users[0].kidneys[i].healthy = true){
+            newkidneys.push({
+                healthy:true
+            })
+        }
+    }
+    users[0].kidneys = newkidneys;
+    res.json({})
+
+})
+
+
+app.listen(3000);
+
+
+
